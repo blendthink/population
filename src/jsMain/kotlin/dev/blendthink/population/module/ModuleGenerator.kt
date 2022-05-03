d633b998eded5182c657cd54812a78a5f77d6eb4
blendthink <info@blendthink.dev>
2022-05-04 03:08:30 +0900

    add Koin Module

---
+++ src/jsMain/kotlin/dev/blendthink/population/module/ModuleGenerator.kt
@@ -0,0 +1,56 @@
+package dev.blendthink.population.module
+
+import dev.blendthink.population.data.repository.PopulationRepository
+import dev.blendthink.population.data.repository.PopulationRepositoryImpl
+import dev.blendthink.population.data.repository.PrefectureRepository
+import dev.blendthink.population.data.repository.PrefectureRepositoryImpl
+import io.ktor.client.*
+import io.ktor.client.engine.js.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+class ModuleGenerator(
+    private val apiKey: String,
+) {
+    fun appModules(): List<Module> {
+        val clientModule = module {
+            single {
+                HttpClient(Js) {
+                    defaultRequest {
+                        url {
+                            protocol = URLProtocol.HTTPS
+                            host = "opendata.resas-portal.go.jp"
+                            path("api/v1/")
+                        }
+                        header("X-API-KEY", apiKey)
+                    }
+                    install(ContentNegotiation) {
+                        json(Json {
+                            prettyPrint = true
+                            isLenient = true
+                        })
+                    }
+                    install(Logging) {
+                        logger = Logger.SIMPLE
+                        level = LogLevel.ALL
+                    }
+                    expectSuccess = true
+                }
+            }
+        }
+
+        val repositoryModule = module {
+            single<PrefectureRepository> { PrefectureRepositoryImpl(get()) }
+            single<PopulationRepository> { PopulationRepositoryImpl(get()) }
+        }
+
+        return listOf(clientModule, repositoryModule)
+    }
+}
