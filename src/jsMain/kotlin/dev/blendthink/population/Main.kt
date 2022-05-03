b9f3dcfbdf7eb71e2c9c0eea13f799a913a6f7d5
blendthink <info@blendthink.dev>
2022-05-04 04:59:24 +0900

    refactor get apiKey

--- src/jsMain/kotlin/dev/blendthink/population/Main.kt
+++ src/jsMain/kotlin/dev/blendthink/population/Main.kt
@@ -1,28 +1,12 @@
 package dev.blendthink.population
 
-import dev.blendthink.population.di.ModuleGenerator
 import dev.blendthink.population.ui.component.PageFooter
 import dev.blendthink.population.ui.component.PageMain
-import dev.blendthink.population.ui.page.showRequireApiKeyPage
 import dev.blendthink.population.ui.style.AppStylesheet
-import kotlinx.browser.window
 import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.renderComposableInBody
-import org.koin.core.context.startKoin
-import org.w3c.dom.url.URLSearchParams
 
 fun main() {
-    val urlParams = URLSearchParams(window.location.search)
-    val apiKey = urlParams.get("apiKey")
-        ?: return showRequireApiKeyPage()
-
-    val moduleGenerator = ModuleGenerator(apiKey)
-    val appModules = moduleGenerator.appModules()
-
-    startKoin {
-        modules(appModules)
-    }
-
     renderComposableInBody {
         Style(AppStylesheet)
 
--- src/jsMain/kotlin/dev/blendthink/population/ui/page/RequireApiKeyPage.kt
+++ src/jsMain/kotlin/dev/blendthink/population/ui/page/RequireApiKeyPage.kt
@@ -1,24 +1,15 @@
 package dev.blendthink.population.ui.page
 
 import androidx.compose.runtime.Composable
-import dev.blendthink.population.ui.component.PageFooter
 import org.jetbrains.compose.web.dom.Main
 import org.jetbrains.compose.web.dom.Span
 import org.jetbrains.compose.web.dom.Text
-import org.jetbrains.compose.web.renderComposableInBody
-
-fun showRequireApiKeyPage() {
-    renderComposableInBody {
-        RequireApiKeyPage()
-    }
-}
 
 @Composable
-private fun RequireApiKeyPage() {
+fun RequireApiKeyPage() {
     Main {
         Span {
             Text("Please set 'apiKey' in the URL parameter.")
         }
     }
-    PageFooter()
 }
