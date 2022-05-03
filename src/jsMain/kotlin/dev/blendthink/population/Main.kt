cf3c554ef9058d732f8f2e86d82e325789ebab1a
blendthink <info@blendthink.dev>
2022-05-04 03:14:34 +0900

    add startKoin implementation

--- src/jsMain/kotlin/dev/blendthink/population/Main.kt
+++ src/jsMain/kotlin/dev/blendthink/population/Main.kt
@@ -1,11 +1,25 @@
 package dev.blendthink.population
 
 import androidx.compose.runtime.*
+import dev.blendthink.population.module.ModuleGenerator
+import kotlinx.browser.window
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
+import org.koin.core.context.startKoin
+import org.w3c.dom.url.URLSearchParams
 
 fun main() {
+    val urlParams = URLSearchParams(window.location.search)
+    val apiKey = urlParams.get("apiKey") ?: throw IllegalArgumentException("Require apiKey")
+
+    val moduleGenerator = ModuleGenerator(apiKey)
+    val appModules = moduleGenerator.appModules()
+
+    startKoin {
+        modules(appModules)
+    }
+
     var count: Int by mutableStateOf(0)
 
     renderComposable(rootElementId = "root") {
