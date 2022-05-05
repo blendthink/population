d01c7f7c5d99a198f3093a28488c0711c8956163
blendthink <info@blendthink.dev>
2022-05-05 21:18:39 +0900

    change px to rem

--- src/jsMain/kotlin/dev/blendthink/population/ui/style/TextStyle.kt
+++ src/jsMain/kotlin/dev/blendthink/population/ui/style/TextStyle.kt
@@ -11,93 +11,93 @@ object TextStyle : StyleSheet() {
     val headline1 by style {
         fontFamily("Robot")
         fontWeight("lighter")
-        fontSize(96.px)
-        letterSpacing((-1.5).px)
+        fontSize(6.cssRem)
+        letterSpacing((-0.009375).cssRem)
     }
 
     val headline2 by style {
         fontFamily("Robot")
         fontWeight("lighter")
-        fontSize(60.px)
-        letterSpacing((-0.5).px)
+        fontSize(3.75.cssRem)
+        letterSpacing((-0.03125).cssRem)
     }
 
     val headline3 by style {
         fontFamily("Robot")
         fontWeight("normal")
-        fontSize(48.px)
-        letterSpacing(0.px)
+        fontSize(3.cssRem)
+        letterSpacing(0.cssRem)
     }
 
     val headline4 by style {
         fontFamily("Robot")
         fontWeight("normal")
-        fontSize(34.px)
-        letterSpacing(0.25.px)
+        fontSize(2.125.cssRem)
+        letterSpacing(0.015625.cssRem)
     }
 
     val headline5 by style {
         fontFamily("Robot")
         fontWeight("normal")
-        fontSize(24.px)
-        letterSpacing(0.px)
+        fontSize(1.5.cssRem)
+        letterSpacing(0.cssRem)
     }
 
     val headline6 by style {
         fontFamily("Robot")
         fontWeight(500)
-        fontSize(20.px)
-        letterSpacing(0.15.px)
+        fontSize(1.25.cssRem)
+        letterSpacing(0.009375.cssRem)
     }
 
     val subtitle1 by style {
         fontFamily("Robot")
         fontWeight("normal")
-        fontSize(16.px)
-        letterSpacing(0.15.px)
+        fontSize(1.cssRem)
+        letterSpacing(0.009375.cssRem)
     }
 
     val subtitle2 by style {
         fontFamily("Robot")
         fontWeight(500)
-        fontSize(14.px)
-        letterSpacing(0.1.px)
+        fontSize(0.875.cssRem)
+        letterSpacing(0.00625.cssRem)
     }
 
     val body1 by style {
         fontFamily("Robot")
         fontWeight("normal")
-        fontSize(16.px)
-        letterSpacing(0.5.px)
+        fontSize(1.cssRem)
+        letterSpacing(0.03125.cssRem)
     }
 
     val body2 by style {
         fontFamily("Robot")
         fontWeight("normal")
-        fontSize(14.px)
-        letterSpacing(0.25.px)
+        fontSize(1.5.cssRem)
+        letterSpacing(0.015625.cssRem)
     }
 
     val button by style {
         fontFamily("Robot")
         fontWeight(500)
-        fontSize(14.px)
-        letterSpacing(1.25.px)
+        fontSize(0.875.cssRem)
+        letterSpacing(0.078125.cssRem)
         textTransform(TextTransform.UPPERCASE)
     }
 
     val caption by style {
         fontFamily("Robot")
         fontWeight("normal")
-        fontSize(12.px)
-        letterSpacing(0.4.px)
+        fontSize(0.75.cssRem)
+        letterSpacing(0.025.cssRem)
     }
 
     val overline by style {
         fontFamily("Robot")
         fontWeight("normal")
-        fontSize(10.px)
-        letterSpacing(1.5.px)
+        fontSize(0.625.cssRem)
+        letterSpacing(0.09375.cssRem)
         textTransform(TextTransform.UPPERCASE)
     }
 }
