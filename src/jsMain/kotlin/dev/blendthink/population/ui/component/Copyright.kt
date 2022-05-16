dd5707965b1152eed4e191683129b6ad69de61e8
blendthink <info@blendthink.dev>
2022-05-17 02:16:36 +0900

    set Copyright TextStyle

--- src/jsMain/kotlin/dev/blendthink/population/ui/component/Copyright.kt
+++ src/jsMain/kotlin/dev/blendthink/population/ui/component/Copyright.kt
@@ -1,6 +1,7 @@
 package dev.blendthink.population.ui.component
 
 import androidx.compose.runtime.Composable
+import dev.blendthink.population.ui.style.TextStyle
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Small
 import org.jetbrains.compose.web.dom.Text
@@ -8,7 +9,7 @@ import org.jetbrains.compose.web.dom.Text
 @Composable
 fun Copyright() {
     Div {
-        Small {
+        Small({ classes(TextStyle.caption) }) {
             Text("Copyright © 2022 blendthink")
         }
     }
