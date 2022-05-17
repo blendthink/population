a2ef9080ad508ef9ad73faf9e1d578cfebb09aa2
blendthink <info@blendthink.dev>
2022-05-18 04:26:41 +0900

    add float extension to StyleScope

--- src/jsMain/kotlin/dev/blendthink/population/ui/extension/StyleScopeExt.kt
+++ src/jsMain/kotlin/dev/blendthink/population/ui/extension/StyleScopeExt.kt
@@ -17,3 +17,14 @@ enum class TextTransform(val value: String) {
     FULL_WIDTH("full-width"),
     FULL_SIZE_KANA("full-size-kana"),
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/CSS/float
+fun StyleScope.float(float: Float) {
+    property("float", float.value)
+}
+
+enum class Float(val value: String) {
+    NONE("none"),
+    LEFT("left"),
+    RIGHT("right"),
+}
